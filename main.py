92cc764af79e8b52ebedc96c8e77dd64d2b295c9
SimpliJessi <mccullough.jessi@gmail.com>
2022-12-30 14:01:37 -0700

    added the main file

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import monster
+
+thisMonster = [vars(monster.mon1), vars(monster.mon2), vars(monster.mon3), vars(monster.mon4)]
+
 size = input("Enter Party Size: ")
 level = input("Enter Player Level: ")
+eDifficulty = int(level) * int(size)
+for i in thisMonster:
+    setattr(i, 'number', eDifficulty / i.cr)
 print(f"Your party has {size} level {level} players.")
 
+difficulty = input("How difficult do you want the encounter?")
 
+print(thisMonster)
